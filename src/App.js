3ea1b9f2e496d8386f5abed23a845b57220004e2
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-07-24 16:26:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+
+  const [step, setStep] = useState(1)
+  const [isOpen, setIsOpen] = useState(false)
+  //  console.log(arr)
+
+  // const step = 1;
+
+  function handlePrevious () {
+    if(step > 1) {
+      setStep(step - 1);
+    }
+    
+    setStep(step - 1)
+  }
+  function handleNext () {
+    if (step < 3)
+    setStep(step++);
+    
+  }
+
+  function handleClose () {
+    setIsOpen(false)
+  }
+  // function handleOpen () {
+  //   set
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {/* if isOpen is true, render component, else "" */}
+      { isOpen ? "component i want to render" : ""}
+      <div>
+        <button
+          onClick={()=> setIsOpen(!isOpen)}
+         className='close'
+         > &times; </button>
+      <div className='steps'>
+        <div className='numbers'>
+          <div className={step >= 1? `active`: ''}>1</div>
+          <div className={step >= 2? `active`: ''}>2</div>
+          <div className={step >= 3? `active`: ''}>3</div>
+        </div>
+        <p className="message"> Step {step}: {messages[step - 1]} </p>
+        <div className='buttons'>
+          <button
+            style={{backgroundColor: '#750f2', color: '#fff'}}
+            onClick={handleNext}
+            > Previous </button>
+          <button
+           style={{backgroundColor: '#750f2', color: '#fff'}}
+           onClick={handleNext}
+           > Next </button>
+        </div> 
+        </div>
+      </div>
     </div>
+
+  ) : (
+    <div>
+      <h1>can't </h1>
+    <button className='close'></button>  
+      
+    </div>
+  )}
   );
 }
 
